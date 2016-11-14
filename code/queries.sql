6ebbdce7f5f2bc694dea8154dda0d70b9d8f6cd1
yzhong9469 <yzhong@oberlin.edu>
2016-11-13 21:48:00 -0500

    Update queries.sql

--- code/queries.sql
+++ code/queries.sql
@@ -19,3 +19,8 @@ AND p1.total >= ALL (SELECT p2.total
 SELECT Categories
 FROM Activities
 WHERE Sports = "swimming"
+
+Select a.name
+From Athletes a inner join Medal m on a.name = m.name AND a.DOB = m.DOB
+where m.gold + m.silver + m.bronze >= ALL ( select m2.gold + m2.silver + m2.bronze
+					from Medal m2)
