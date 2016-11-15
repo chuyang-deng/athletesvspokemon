078d17f7839b934d01f18c518eb2ff01fdf77547
yzhong9469 <yzhong@oberlin.edu>
2016-11-14 22:12:03 -0500

    Update queries.sql

--- code/queries.sql
+++ code/queries.sql
@@ -1,7 +1,7 @@
 -- Athletes (name, DOB, height, weight, gender, sports, event)
 -- Medal (name, DOB, gold, silver, bronze)
 -- Activities (Sports, type)
--- Pokemon (name, HP, total, speed, defense, attack, gender, type)
+-- Pokemon (name, HP, total, speed, defense, attack, gender, type, image_NO)
 
 -- Who is the best pokemon?
 SELECT p1.name
@@ -16,9 +16,17 @@ AND p1.total >= ALL (SELECT p2.total
 					 FROM Pokemon p2)
 
 -- What will swimmers do in Pokemon?
-SELECT Categories
+SELECT type
 FROM Activities
 WHERE Sports = "swimming"
+
+--use the type above, select some sample pokemon, picture will be linked by image_NO
+--using AWS S3, more description will be provided by noSQL database and a json file
+--which has not been set up yet
+Select p.name
+from Pokemon p
+where p.type = "above type" AND ROWNUM < 10
+
 
 -- who is the best athletes
 Select m.name
@@ -31,3 +39,17 @@ Select a.name
 From Althetes a inner join Medal m on a.name = m.name AND a.DOB = m.DOB
 where a.sports = "swimming" AND 5*m.gold + 3*m.silver + m.bronze >= ALL ( select 5*m2.gold + 3*m2.silver + m2.bronze
 					from Medal m2)
+					
+-- Given two atheletes name, show which one would win
+Select 5*m.gold + 3*m.silver + m.bronze
+From Medal m
+where where m.name = "name1 or name2"  
+-- then in NodeJS, compare the number and print out the name with the higher number
+-- we will then use our matching algorithm to select two pokemon for each athelets
+--then
+Select p.total
+from Pokemon p
+where p.name = "above name"
+-- then in NodeJS, compare two number and print out the name with the higher total
+
+
