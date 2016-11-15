966c22a4cdf571edef6b70a0bc8782c183acbafa
yzhong9469 <yzhong@oberlin.edu>
2016-11-14 22:02:38 -0500

    Update queries.sql

--- code/queries.sql
+++ code/queries.sql
@@ -1,7 +1,7 @@
--- Athlets (name, DOB, height, weight, gender, sports)
+-- Athletes (name, DOB, height, weight, gender, sports, event)
 -- Medal (name, DOB, gold, silver, bronze)
--- Activities (Sports, Categories)
--- Pokemon (name, HP, total, speed, defense, attack, gender, category)
+-- Activities (Sports, type)
+-- Pokemon (name, HP, total, speed, defense, attack, gender, type)
 
 -- Who is the best pokemon?
 SELECT p1.name
@@ -11,7 +11,7 @@ WHERE p1.total >= ALL (SELECT p2.total
 
 SELECT p1.name
 FROM Pokemon p1
-WHERE p1.category = "water"
+WHERE p1.type = "water"
 AND p1.total >= ALL (SELECT p2.total
 					 FROM Pokemon p2)
 
@@ -20,7 +20,14 @@ SELECT Categories
 FROM Activities
 WHERE Sports = "swimming"
 
+-- who is the best athletes
+Select m.name
+From Medal m
+where 5*m.gold + 3*m.silver + m.bronze >= ALL ( select 5*m2.gold + 3*m2.silver + m2.bronze
+					from Medal m2)
+					
+---- in certain category
 Select a.name
-From Athletes a inner join Medal m on a.name = m.name AND a.DOB = m.DOB
-where m.gold + m.silver + m.bronze >= ALL ( select m2.gold + m2.silver + m2.bronze
+From Althetes a inner join Medal m on a.name = m.name AND a.DOB = m.DOB
+where a.sports = "swimming" AND 5*m.gold + 3*m.silver + m.bronze >= ALL ( select 5*m2.gold + 3*m2.silver + m2.bronze
 					from Medal m2)
