a41aa6782a1783abfb650960357ffb365422b429
Chuyang Deng <chuyangdeng@Chuyangs-MacBook-Pro.local>
2016-12-06 10:55:12 -0500

    scraped pokemon and athletes popularity data from google search

--- pokemon/routes/guessgame.js
+++ pokemon/routes/guessgame.js
@@ -1,10 +1,39 @@
 var express = require('express');
 var router = express.Router();
 
+var mysql = require('mysql');
+
+var settings = require('../settings.json');
+var connection = mysql.createConnection({
+    host: settings.address,
+    user: settings.user,
+    password: settings.password,
+    database: settings.database
+})
+
+/**
+ * Get 2 random tuples from pokemon table
+ */
+
+function query_random_pokemon(res) {
+    var query = "SELECT name, image_no, total FROM PokemonFull ORDER BY RAND() LIMIT 2";
+    connection.query(query, function(err, rows, fileds) {
+        if (err) console.log(err);
+        else output_result(res, rows);
+    });
+}
+
+function output_result(res, result) {
+    res.render('guessgame',
+        {
+            title: 'Guess Game',
+            result: result
+        });
+}
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-    res.render('guessgame', {
-        title: 'Guess Game',}); // render a view
+router.get('/', function(req, res) {
+    query_random_pokemon(res); // render the query result
 }); // use index.ejs as view, also pass additional information, in this case, title: Express
 
 module.exports = router;
